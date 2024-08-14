d8d63b18d63d0e9f8240761a4dce444aefce2568
Yitzkak <bevinchi@gmail.com>
2024-08-14 14:49:05 +0100

    The second step is complete

--- api/views.py
+++ api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import AudioFile
+from .serializers import AudioFileSerializer
 
-# Create your views here.
+from django.shortcuts import render
+from .forms import AudioFileForm
+
+class AudioFileViewSet(viewsets.ModelViewSet):
+    queryset = AudioFile.objects.all()
+    serializer_class = AudioFileSerializer
+
+
+def upload_view(request):
+    success_message = None
+    if request.method == 'POST':
+        form = AudioFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            success_message = "File uploaded successfully!"
+    else:
+        form = AudioFileForm()
+    return render(request, 'upload.html', {'form': form, 'success_message': success_message})
