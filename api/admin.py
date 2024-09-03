7f37f160f45feb85b0132f163991ec4326ce63fa
Yitzkak <bevinchi@gmail.com>
2024-09-03 19:49:51 +0100

    File upload working, file saving, file retrieval working.

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Transcription, AudioFile
 
-# Register your models here.
+@admin.register(AudioFile)
+class AudioFileAdmin(admin.ModelAdmin):
+    list_display = ('file', 'original_filename', 'uploaded_at')
+
+@admin.register(Transcription)
+class TranscriptionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'original_audio_filename', 'transcription_text', 'audio_file', 'created_at', 'updated_at')
