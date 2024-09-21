d2c9b9a17f1368b20719b9e32291643f6b00dd94
Yitzkak <bevinchi@gmail.com>
2024-09-21 23:41:09 +0100

    Not serious changes

--- transcription_project/settings.py
+++ transcription_project/settings.py
@@ -32,8 +32,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", "default-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = os.getenv("DEBUG", "False").lower() in ['true', '1']
+DEBUG = True
+#DEBUG = os.getenv("DEBUG", "False").lower() in ['true', '1']
 
 ALLOWED_HOSTS = ['intelliscribe.applikuapp.com', 'localhost', '127.0.0.1']
 
