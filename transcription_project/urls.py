b651805c6adcf17911555f62e117fe963d2d9f87
Yitzkak <bevinchi@gmail.com>
2024-09-23 17:10:40 +0100

    Test page for backend and frontend added

--- transcription_project/urls.py
+++ transcription_project/urls.py
@@ -5,6 +5,7 @@ from api.views import AudioFileViewSet
 from api.views import upload_view
 from api.views import TranscriptionListCreateView, TranscriptionDetailView
 from api.views import check_task_status
+from api.views import tpage
 
 router = DefaultRouter()
 router.register(r'audio', AudioFileViewSet)
@@ -16,6 +17,7 @@ urlpatterns = [
     path('api/transcriptions/', TranscriptionListCreateView.as_view(), name='transcription-list-create'),
     path('api/transcriptions/<int:pk>/', TranscriptionDetailView.as_view(), name='transcription-detail'),
     path('api/task-status/<str:task_id>/', check_task_status, name='task-status'),
+    path('tpage', tpage, name='tpage'), # for testing
 ]
 
 from django.conf import settings
